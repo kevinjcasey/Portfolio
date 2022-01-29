c9af1a9ecbd5246914af2ef1f3d11faf9bec2660
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-01-29 14:03:08 -0500

    Added grow effect on socials icons; tested some color schemes on about page and header/footer

---
+++ images/Kevin Casey Portfolio_files/app.js
@@ -0,0 +1,22 @@
+console.log("The jQuery is linked!");
+
+// .flip to make images flip to reveal text 
+// .trigger on click - may not need this if using on click?
+
+$(() => {
+
+// ============= hamburger nav menu =================
+
+
+const $hamburgerButton = $('.hamburger-button')
+const $navLinks = $('.nav-links')
+
+$hamburgerButton.on('click', () => {
+    $('.nav-links').toggle(600).appendTo('.header')
+})
+
+
+
+
+
+})
