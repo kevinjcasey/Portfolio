e80ace7c6ed4f6b658b63d2a5fb16b357c776983
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-01-28 19:43:38 -0500

    Added hamburger menu button CSS; started jQuery

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ console.log("The jQuery is linked!");
 
 // .flip to make images flip to reveal text 
 // .trigger on click - may not need this if using on click?
+
+// ============= hamburger nav menu =================
+
+const $hamburgerButton = ('.hamburger-button')
+const $navLinks = ('.nav-links')
+
+$hamburgerButton.on('click' = () =>
+
+)
