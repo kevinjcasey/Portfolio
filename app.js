bf8be3c9388823e4c8930dc685fc48c651b6320c
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-01-31 21:03:54 -0500

    Added fonts to all pages; updated font colors

--- app.js
+++ app.js
@@ -7,31 +7,31 @@ $(() => {
 
 // ============ modal pop-up on homepage =================
 
-const $modal = $('#modal')
-const $closeButton = $('#close')
-const $claim = $('#claim')
+    // const $modal = $('#modal')
+    // const $closeButton = $('#close')
+    // const $claim = $('#claim')
 
-const openModal = () => {
-    $modal.css('display', 'block')
-}
-const closeModal = () => {
-    $modal.css('display', 'none')
-}
+    // const openModal = () => {
+    //     $modal.css('display', 'block')
+    // }
+    // const closeModal = () => {
+    //     $modal.css('display', 'none')
+    // }
 
-$closeButton.on('click', closeModal)
-$claim.on('click', closeModal)
+    // $closeButton.on('click', closeModal)
+    // $claim.on('click', closeModal)
 
-setTimeout(openModal, 5000);
+    // setTimeout(openModal, 6000);
 
 // ============= hamburger nav menu ======================
 
 
-const $hamburgerButton = $('.hamburger-button')
-const $navLinks = $('.nav-links')
+    const $hamburgerButton = $('.hamburger-button')
+    const $navLinks = $('.nav-links')
 
-$hamburgerButton.on('click', () => {
-    $navLinks.toggle(600).appendTo('.header')
-})
+    $hamburgerButton.on('click', () => {
+        $navLinks.toggle(600).appendTo('.header')
+    })
 
 
 // const $castleBattle = $('.castle-battle')
@@ -40,44 +40,64 @@ $hamburgerButton.on('click', () => {
 
 // ============ resume accordion =========================
 
-const accordionFunction = () => {
-    $('#accordion-1').accordion( {
-        collapsible: true,
-        active: false
-    })
-    $('#accordion-2').accordion( {
-        collapsible: true,
-        active: false
-    })
-}
+    const accordionFunction = () => {
+        $('#accordion-1').accordion( {
+            collapsible: true,
+            active: false
+        })
+        $('#accordion-2').accordion( {
+            collapsible: true,
+            active: false
+        })
+    }
 
-accordionFunction()
+    accordionFunction()
 
 // ============ contact page text inputs =================    
    
-const $contactForm = $('<section>').attr('id', 'inputs')
-$contactForm.appendTo('.contact-form')
-$contactForm.append('<input id="name" type="text" placeholder="Your Name"/>')
-$contactForm.append('<input id="email" type="text" placeholder="Your Email"/>')
-$contactForm.append('<input id="phone" type="text" placeholder="Phone"/>')
-$contactForm.append('<input id="company" type="text" placeholder="Company"/>') 
-$contactForm.append('<input id="subject" type="text" placeholder="Subject"/>')
+    const $contactForm = $('<section>').attr('id', 'inputs')
+    $contactForm.appendTo('.contact-form')
+    $contactForm.append('<input id="name" type="text" placeholder="Your Name"/>')
+    $contactForm.append('<input id="email" type="text" placeholder="Your Email"/>')
+    $contactForm.append('<input id="phone" type="text" placeholder="Phone"/>')
+    $contactForm.append('<input id="company" type="text" placeholder="Company"/>') 
+    $contactForm.append('<input id="subject" type="text" placeholder="Subject"/>')
 
-const $messageBox = $('<section>').attr('id', 'message-box')
-$messageBox.appendTo('.contact-form')
-$messageBox.append('<textarea id="message" placeholder="Message"/>')
+    const $messageBox = $('<section>').attr('id', 'message-box')
+    $messageBox.appendTo('.contact-form')
+    $messageBox.append('<textarea id="message" placeholder="Message"/>')
+
+    const $submitButton = $('<section>').attr('id', 'submit')
+    $submitButton.appendTo('.contact-form')
+    $submitButton.append('<input id="submit-button" type="submit" value="Send Message"/>')
+
+// ============ contact page send message modal =================  
 
 
-const $contactModal = $('#contact-modal')
-// when "send message" button is clicked, have modal pop up saying "Thank you for reaching out!"
+    const $sendMessage = () => {
+        $('#contact-modal').css('display', 'block')
 
-const $submitButton = $('<section>').attr('id', 'submit')
-$submitButton.appendTo('.contact-form')
-$submitButton.append('<input id="submit-button" type="submit" value="Send Message"/>')
+        const $contactModal = $('<div>').attr('id', 'modal-2')
+        $contactModal.appendTo('#contact-modal')
+        $('<h3>').text('Thank you for reaching out!').appendTo($contactModal) 
+        $('<h4>').text('You will hear back from me in three months after my bootcamp is finished.').appendTo($contactModal)
 
-// $submitButton.on('click', () => {
-//     $contactModal()
-// })
+        const $closeBtn = $('<button>').attr('id','close-btn').text('Hooray!')
+        $closeBtn.appendTo($contactModal)
+        $closeBtn.on('click', () => {
+            $closeContactModal
+        // reloads contact page 
+           location.reload()
+        })
+    }   
+
+    $submitButton.on('click', () => {
+        $sendMessage()
+    })
+
+    const $closeContactModal = () => {
+        $('#contact-modal').css('display', 'none')
+    }
 
 })
 
